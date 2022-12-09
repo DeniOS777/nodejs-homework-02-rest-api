274c959b12b4c4f25df81df7441cc5b618bec0ba
DeniOS777 <Denios777@gmail.com>
2022-12-09 19:12:39 +0200

    added get listContacts with database

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
-const { contactsSchema } = require("../../schemas");
-const { validation } = require("../../middlewares");
+const { contactsSchema } = require('../../schemas');
+const { validation } = require('../../middlewares');
 
-const contactsOperations = require("../../models/contacts");
+const contactsOperations = require('../../models/contacts');
 
 const router = express.Router();
 
-router.get("/", async (_, res, next) => {
+router.get('/', async (_, res, next) => {
   try {
     const contacts = await contactsOperations.listContacts();
     res.json({
-      message: "success",
+      message: 'success',
       code: 200,
       data: {
         contacts,
@@ -23,18 +23,18 @@ router.get("/", async (_, res, next) => {
   }
 });
 
-router.get("/:contactId", async (req, res, next) => {
+router.get('/:contactId', async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const contact = await contactsOperations.getContactById(contactId);
 
     if (!contact) {
       return res.status(404).json({
-        message: "Not found",
+        message: 'Not found',
       });
     }
     res.status(200).json({
-      message: "success",
+      message: 'success',
       code: 200,
       data: {
         contact,
@@ -45,7 +45,7 @@ router.get("/:contactId", async (req, res, next) => {
   }
 });
 
-router.post("/", validation(contactsSchema), async (req, res, next) => {
+router.post('/', validation(contactsSchema), async (req, res, next) => {
   try {
     const body = {
       id: uuidv4(),
@@ -53,7 +53,7 @@ router.post("/", validation(contactsSchema), async (req, res, next) => {
     };
     const contact = await contactsOperations.addContact(body);
     res.status(201).json({
-      message: "success",
+      message: 'success',
       code: 201,
       data: {
         contact,
@@ -64,18 +64,18 @@ router.post("/", validation(contactsSchema), async (req, res, next) => {
   }
 });
 
-router.delete("/:contactId", async (req, res, next) => {
+router.delete('/:contactId', async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const contact = await contactsOperations.removeContact(contactId);
     if (!contact) {
       return res.status(404).json({
-        message: "Not found",
+        message: 'Not found',
       });
     }
     res.status(200).json({
-      status: "success",
-      message: "contact deleted",
+      status: 'success',
+      message: 'contact deleted',
       code: 200,
       data: {
         contact,
@@ -86,22 +86,39 @@ router.delete("/:contactId", async (req, res, next) => {
   }
 });
 
-router.put("/:contactId", validation(contactsSchema), async (req, res, next) => {
+router.put(
+  '/:contactId',
+  validation(contactsSchema),
+  async (req, res, next) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await contactsOperations.updateContact(
+        contactId,
+        req.body
+      );
+      if (!contact) {
+        return res.status(404).json({
+          message: 'Not found',
+        });
+      }
+      res.status(200).json({
+        message: 'success',
+        code: 200,
+        data: {
+          contact,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.patch('/:contactId/favorite', async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const contact = await contactsOperations.updateContact(contactId, req.body);
-    if (!contact) {
-      return res.status(404).json({
-        message: "Not found",
-      });
-    }
-    res.status(200).json({
-      message: "success",
-      code: 200,
-      data: {
-        contact,
-      },
-    });
+    return contact;
   } catch (error) {
     next(error);
   }
--- schemas/contactsSchema.js
+++ schemas/contactsSchema.js
@@ -1,11 +1,15 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
 const contactsSchema = Joi.object({
   name: Joi.string().min(2).max(30).required(),
   email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net', 'org', 'uk', 'ca'] },
+    })
     .required(),
   phone: Joi.string().min(6).required(),
+  favorite: Joi.boolean().default(false),
 });
 
 module.exports = contactsSchema;
